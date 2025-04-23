a8838c63b7fe0f0c6867870255dbfe549746ed72
Hareni1103 <hareni1103@gmail.com>
2025-04-23 23:29:50 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1eCfX-EUN1fUETPCjY7vJCNINMiNKbxyI
 """
 
-pip install streamlit
 
 # Import required libraries
 import pandas as pd
